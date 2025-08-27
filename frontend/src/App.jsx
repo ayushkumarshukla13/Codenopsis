0a6de32731ba2d518fb2ae8cd0066c32f0a1b19a
Ayush Kumar Shukla <shuklaayushlive@gmail.com>
2025-08-27 21:03:11 +0530

    store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,21 @@ import {Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Homepage from "./pages/Homepage";
+import {checkAuth} from "./authSlice"
+import { useDispatch,useSelector} from "react-redux";
+import { useEffect } from "react";
 
 function App(){
+
+  // code likhna isAuthentciated
+  const {isAuthenticated} =  useSelector((state)=>state.auth);
+  const dispatch = useDispatch();
+
+
+  useEffect(()=>{
+   dispatch(checkAuth());
+  },[isAuthenticated]);
+  
 
   return(
   <>
