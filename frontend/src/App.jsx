4790f714a600ff17761c96cb68cb0a29b03149d2
Ayush Kumar Shukla <shuklaayushlive@gmail.com>
2025-08-27 21:34:23 +0530

    Admin Panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,25 @@ import Homepage from "./pages/Homepage";
 import { useDispatch, useSelector } from 'react-redux';
 import { checkAuth } from "./authSlice";
 import { useEffect } from "react";
+import AdminPanel from "./pages/AdminPanel";
 
 function App(){
   
   const dispatch = useDispatch();
-  const {isAuthenticated} = useSelector((state)=>state.auth);
+  const {isAuthenticated,user,loading} = useSelector((state)=>state.auth);
 
-  
+  // check initial authentication
   useEffect(() => {
     dispatch(checkAuth());
   }, [dispatch]);
 
 
+  
+  if (loading) {
+    return <div className="min-h-screen flex items-center justify-center">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>;
+  }
 
   return(
   <>
@@ -24,6 +31,15 @@ function App(){
       <Route path="/" element={isAuthenticated ?<Homepage></Homepage>:<Navigate to="/signup" />}></Route>
       <Route path="/login" element={isAuthenticated?<Navigate to="/" />:<Login></Login>}></Route>
       <Route path="/signup" element={isAuthenticated?<Navigate to="/" />:<Signup></Signup>}></Route>
+      <Route path="/admin" element={<AdminPanel/>}></Route>
+      {/* <Route 
+        path="/admin" 
+        element={
+          isAuthenticated && user?.role === 'admin' ? 
+            <AdminPanel /> : 
+            <Navigate to="/" />
+        } 
+      /> */}
     </Routes>
   </>
   )
