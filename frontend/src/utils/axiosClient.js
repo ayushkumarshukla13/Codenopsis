5415a2615e2c17067ab59c13e58db5da221b5aac
Ayush Kumar Shukla <shuklaayushlive@gmail.com>
2025-09-05 14:51:17 +0530

    cors

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://codenopsis.vercel.app/',
+    baseURL: 'https://codenopsis.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
