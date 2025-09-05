e2c8355f81d6355e7050806f2b67bae800ef8f9d
Ayush Kumar Shukla <shuklaayushlive@gmail.com>
2025-09-05 15:42:11 +0530

    render

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://codenopsis.vercel.app',
+    baseURL: 'https://codenopsis.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
